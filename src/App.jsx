eec5cbfccd6dead8b93aaf25d5d8ef0390b83657
Hyeonho Kang <hyeonhokang10@kakao.com>
2024-10-13 16:10:25 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div class="flex gap-3">
+        <div class="flex-1 w-64 bg-base-300 p-3 rounded-xl">
+          02
+        </div>
+        <div class="flex-1 w-64 bg-base-300 p-3 rounded-xl">
+          03
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
