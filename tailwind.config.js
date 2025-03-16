2eaa282e0b03b60026c3bb2865609afaa9dafb90
Hyeonho Kang <hyeonhokang10@kakao.com>
2025-03-16 14:22:47 +0900

    Preparing tailwind 4 upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,5 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-  daisyui: {
-    themes: [
-      {'light': {
-          "base-200": "#D09EF8",
-          "base-300": "#BB8EDF"
-        }
-      }
-    ]
-  }
 }
 
